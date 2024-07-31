6a80e8455dbc3db70323744831e4ab50135fcfd9
onja-phi <153230779+onja-phi@users.noreply.github.com>
2024-07-31 09:10:36 +0200

    feat: SongDuration.java file added

---
+++ SongDuration.java
@@ -0,0 +1,10 @@
+package com.examen;
+
+public class SongDuration {
+    public static void main(String[] args){
+        long minutes = duration.toMinutesPart();
+        long seconds = duration.toSecondsPart();
+
+        System.out.printf("Duration: ", minutes, seconds);
+    }
+}
